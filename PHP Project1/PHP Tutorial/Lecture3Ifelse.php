582c9c2171545d6119eed7c691d73ae920093a1c
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-08-04 21:48:43 +0530

    First Commit

---
+++ PHP Project1/PHP Tutorial/Lecture3Ifelse.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lecture 3</title>
+</head>
+<body>
+    <h1> This is Third Lecture for PHP Course</h1>
+    <H2> How to Use IfElse Condition in PHP </H2>
+    <?php
+    $var = 5;
+    if($var>=10){
+        echo " Greater than 10";
+    }
+    else{
+        echo "Less Than 10";
+    }
+    
+    
+    ?>
+    
+</body>
+</html>
