a4c7bb94b9c7e0d6a5d54b2e8fded92f3ebdc18d
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-08-05 15:59:43 +0530

    First Commit for PHP

---
+++ PHP Project1/PHP Tutorial/Lecture005Loops.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lecture 5 </title>
+</head>
+<body>
+    <h1> This is 5th Lecture for PHP Course</h1>
+    <H2> How to Use Loops(While,Do-While,For) in PHP </H2>
+    <h2> Print Number 1 to 6 Using For Loop </h2>
+    <?php
+/*
+//While Loop
+$var = 5;
+while ($var <= 10){
+    echo "Number: $var <br>";
+    $var++;
+}
+
+// Do while Loop
+$var = 11;
+do{
+    echo "Number: $var <br>";
+    $var++;
+} while($var<=15)
+*/
+// For Loop
+
+for ($var = 1; $var<=10; $var++){
+    echo "Number: $var <br>";
+}
+
+?>
+   
+</body>
+</html>
+
