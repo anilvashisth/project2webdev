9c13f10b21b2794d90a31897dab4f699a3a68774
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-08-05 16:01:36 +0530

    First Commit for PHP Function
    Learn How to Use Function in PHP

---
+++ PHP Project1/PHP Tutorial/Lecture006Functions.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lecture 6</title>
+</head>
+<body>
+    <h1> 6th Lecture from PHP Course</h1>
+    <h2> HOw to use Function Using PHP</h2>
+<?php
+    
+function name(){
+    for ($var = 1; $var<=6; $var++){
+        echo "Number: $var <br>";
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+name();
+echo "i m going to run it later <br> ";
+
+name();
+
+?>
+
+</body>
+</html>
