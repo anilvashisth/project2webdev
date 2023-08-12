dc75132bea3c8334a4f39d2d87dcb52dee74f21c
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-08-12 21:57:24 +0530

    First Commit

---
+++ PHP Tutorial new/post/registration.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>input with post</h1>
+    Welcome 
+    <?php 
+    echo $_POST["name"];
+     ?>!
+    Your email address is 
+    <?php 
+    echo $_POST["email"]; 
+    ?> 
+    
+</body>
+</html>
