ea90f16d021c98f6e7c07d307439e613c0859ab4
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-09-03 23:36:26 +0530

    First Commit

---
+++ LearnwithSimpliAK/tutorial1.php
@@ -0,0 +1,85 @@
+<?php
+/*
+$str="Hello and learn with AK Sir";
+$num=19;
+$float=18.24;
+
+echo "<h1>" .$str. "</h1>";
+echo "<h2>" .$num. "</h2>";
+echo "<h3>" .$float. "</h3>";
+// Array
+$laptopbrand = array("Hp","Dell","Sony");
+echo var_dump($laptopbrand) ;
+
+// For Constant data 
+define("laptops",["Sony","Hp","Dell","lenovo"]);
+echo laptops[2];
+
+//Condition if
+$var = 10;
+if($var>9){
+    echo "less than ",$var;
+}else{
+    echo "More than ",$var;
+}
+
+// WAP to find a no is divisible by 2,3
+$var = 11;
+if($var%2==0 && $var%3==0){
+    echo "Divisible by both";
+
+}elseif($var%2==0){
+    echo "Divisible by 2";
+}elseif($var%3==0){
+    echo "Divisible by 3";
+}else{
+    echo "Not Divisible by both";
+}
+
+//Switch case
+
+$var=40;
+switch($var){
+    case 1:
+        echo "Easy to Play";
+    break;
+    case 2:
+        echo "Medium to Play";
+    break;
+    case 3:
+        echo "Hard to Play";
+    break;
+    case 4:
+        echo "Extreme Hard to Play";
+    break;
+    default:
+        echo "Invalid input";
+    break;    
+}
+
+//Loops in PHP (While, DO-While, For)
+$var = 2;
+
+while($var <= 5){
+    echo "Number: $var", "<br>";
+    $var++;
+} 
+
+// Do While loop
+$var=12;
+do{
+    echo "Number: $var", "<br>";
+    $var++;
+
+}
+while($var <= 5)
+*/
+//For Loops
+
+for($i=1;$i<=5;$i++){
+    echo "Number: $i <br>";
+}
+
+
+
+?>
