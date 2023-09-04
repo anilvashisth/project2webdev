1868f967e1826f9cc1bed468a9e566a91251ed82
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-09-04 22:22:42 +0530

    First Commit

---
+++ class.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOPS Concept in PHP</title>
+</head>
+<body>
+    <h1>OOPS Piller</h1>
+    <h2>Abstruction</h2>
+    <h2>Encapsulation</h2>
+    <h3>inheritence</h3>
+    <h3>Polymorphisum</h3>
+<?PHP
+$var =10;
+echo "Number : $var";
+
+?>
+    
+</body>
+</html>
