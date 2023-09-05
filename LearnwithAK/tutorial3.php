b2e29be40dc8bb51f8e0e6555b0a9e4b8b58bd6e
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-09-05 20:11:00 +0530

    First Commit

---
+++ LearnwithAK/tutorial3.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOPS Concept in PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>OOPS Piller</h1>
+    <p> 
+        It is the most popular programming model among developers. <br>
+        It is well suited for programs that are large, complex, and actively updated or maintained. <br>
+        It simplifies software development and maintenance by providing Concepts (Given Below)<br>
+    </p>
+    <p>
+        <h3>Imp Points </h3>
+            Object is refefferd to as a Data Field (Unique Attribute and Behavior)<br>
+            Everything is an object.<br>
+            Every object is an instance of a class.<br>
+            Developer manipulates objects that uses message passing.<br>
+            The class contains the attribute and behaviour associated with an object.<br>
+    </p>
+    <h3>Abstraction</h3>
+    <h2>Encapsulation</h2>
+    <h2>Inheritance</h2>
+    <h3>Polymorphism</h3>
+<?PHP
+$var =10;
+echo "Number : $var";
+
+?>
+    
+</body>
+</html>
